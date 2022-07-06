06240533babcc70892dc353a6706d000a37344ff
The Vinh <vinhnguyen@descartes.network>
2022-07-06 17:14:22 +0700

    Update by convention

--- src/view/nftPlugin/cardNFT/imageNFT.tsx
+++ src/view/nftPlugin/cardNFT/imageNFT.tsx
@@ -2,13 +2,13 @@ import { Avatar } from 'antd'
 
 import IMAGE_DEFAULT from 'static/images/nft-default.svg'
 
-type ImageNFTProps = {
+export type ImageNFTProps = {
   src?: string
   size?: number
 }
 
-const ImageNFT = ({ src, size = undefined }: ImageNFTProps) => {
-  return <Avatar shape="square" src={src || IMAGE_DEFAULT} size={size} />
+const ImageNFT = ({ src = IMAGE_DEFAULT, size }: ImageNFTProps) => {
+  return <Avatar shape="square" src={src} size={size} />
 }
 
 export default ImageNFT
