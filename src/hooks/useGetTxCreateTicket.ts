97d8b0c96db57094bd4d4565e1fd743420d014ee
tuphan-dn <tuphan@descartes.network>
2022-09-07 21:30:01 +0700

    _

--- src/hooks/useGetTxCreateTicket.ts
+++ src/hooks/useGetTxCreateTicket.ts
@@ -1,6 +1,7 @@
 import { useCallback } from 'react'
-import { Connection, PublicKey } from '@solana/web3.js'
+import { Connection, Keypair, PublicKey } from '@solana/web3.js'
 import { AnchorProvider } from '@project-serum/anchor'
+import NodeWallet from '@project-serum/anchor/dist/cjs/nodewallet'
 import { rpc, useWalletAddress } from '@sentre/senhub'
 import LuckyWheelProgram from '@sentre/lucky-wheel-core'
 
@@ -10,27 +11,30 @@ export const useGetTxCreateTicket = () => {
   const walletAddress = useWalletAddress()
 
   const getTxCreateTicket = useCallback(async () => {
+    const ticket = new Keypair()
+    const ticketSigner = new NodeWallet(ticket)
     const connection = new Connection(rpc, 'confirmed')
-
-    const wrapWallet = {
+    const wrappedWallet = {
       publicKey: new PublicKey(walletAddress),
       signAllTransactions: window.sentre.wallet.signAllTransactions,
       signTransaction: window.sentre.wallet.signTransaction,
     }
-    const provider = new AnchorProvider(connection, wrapWallet, {
+    const provider = new AnchorProvider(connection, wrappedWallet, {
       commitment: 'confirmed',
       skipPreflight: true,
     })
     const program = new LuckyWheelProgram(provider, configs.lottery.programId)
     const { tx } = await program.initializeTicket({
       campaign: new PublicKey(configs.lottery.campaignId),
+      ticket,
       sendAndConfirm: false,
     })
-    tx.feePayer = tx.feePayer || wrapWallet.publicKey
+    tx.feePayer = tx.feePayer || wrappedWallet.publicKey
     tx.recentBlockhash =
       tx.recentBlockhash ||
-      (await provider.connection.getLatestBlockhash()).blockhash
-    return tx
+      (await provider.connection.getLatestBlockhash('confirmed')).blockhash
+    const partiallySignedTx = await ticketSigner.signTransaction(tx)
+    return partiallySignedTx
   }, [walletAddress])
 
   return getTxCreateTicket
